00b5e3a55fe5374d69e004922fdb7948578b9cbe
Piyush Kanojiya <raat131221@gmail.com>
2026-09-22 23:54:47 +0530

    feat: add selected work to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import SelectedWork from "@/components/SelectedWork";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <SelectedWork />
     </main>
   );
 }
