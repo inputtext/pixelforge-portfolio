c960bd032f22917ce43e73d218df8ef522e461e6
Piyush Kanojiya <raat131221@gmail.com>
2026-09-23 00:25:44 +0530

    rebuild PixelForge visual system

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import Contact from "@/components/Contact";
 import Experiments from "@/components/Experiments";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Process from "@/components/Process";
-import Contact from "@/components/Contact";
 import SelectedWork from "@/components/SelectedWork";
 
 export default function Home() {
