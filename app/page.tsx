43877f25cba853fae01f6ad61d2e8403d647fdae
Piyush Kanojiya <raat131221@gmail.com>
2026-09-23 14:55:22 +0530

    Mount GSAP animation controller

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Contact from "@/components/Contact";
 import Experiments from "@/components/Experiments";
 import Hero from "@/components/Hero";
+import GSAPAnimations from "@/components/GSAPAnimations";
 import Navbar from "@/components/Navbar";
 import Process from "@/components/Process";
 import SelectedWork from "@/components/SelectedWork";
@@ -8,6 +9,7 @@ import SelectedWork from "@/components/SelectedWork";
 export default function Home() {
   return (
     <main>
+      <GSAPAnimations />
       <Navbar />
       <Hero />
       <SelectedWork />
